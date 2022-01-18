2c7e80c235486f0b879818d17f604dbf533dac51
Amber Zimmerman <ajz1221@gmail.com>
2022-01-18 14:55:59 -0700

    adding pseudo code to javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Coding Quiz Challenge start page
+ * Start Quiz button
+ * Timer that starts counting down
+ * At least five questions
+ * Four answers for each question, three incorrect, one correct
+ * An alert to tell you when a question was right
+ * An alert to tell you when a question was wrong
+ * Time subtracted when an answer is wrong
+ * When timer runs out a place to enter your initials
+ * Submit button for initials
+ * Highscore page with initials and score
+ * Button to go back
+ * Button to clear score
+ */
+
+let quizQuestion = ["Commonly used data types do NOT include", "The condition of an if/else statement is enclosed within a", "A useful tool for debugging", "What is the proper way to declare a variable in JavaScript" "What is NOT a datatype for a variable"]
