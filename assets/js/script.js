62c38aab052788eebc6f88ac442597d6309bec42
Amber Zimmerman <ajz1221@gmail.com>
2022-05-08 12:12:47 -0600

    changing questions and answers to being about flamingos. Cleaning up some un used code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,14 +47,13 @@ let quizQuestions = [
     // index of 0
     // question and answers 1
     numb: 1,
-    question:
-      "Complete the title of this Salvador Dali Painting: Gala Contemplating the Mediterranean Sea which at Twenty Meters Becomes the Portrait of ____",
-    answer: "Abraham Lincoln",
+    question: "A flamingo typically weighs ____",
+    answer: "4 to 8 pounds",
     options: [
-      "Thomas Edison",
-      "Alexander Hamilton",
-      "Abraham Lincoln",
-      "Sigmund Freud",
+      "20 to 25 pounds",
+      "8 to 12 pounds",
+      "4 to 8 pounds",
+      "15 to 20 pounds",
     ],
   },
   {
@@ -62,49 +61,37 @@ let quizQuestions = [
     // question and answers 2
     numb: 2,
     question:
-      "Caravaggio employed this technique during the Baroque period and made it a dominant stylistic element",
-    answer: "Tenebrism",
-    options: ["Tempera painting", "Impasto", "Tenebrism", "Grisaille"],
+      "The ancient people of Rome used to eat this part of the flamingo and considered it a delicacy?",
+    answer: "Tongue",
+    options: ["Tongue", "Stomach", "Eyes", "Leg"],
   },
   {
     // question and answers 3
     numb: 3,
     question:
-      "Between 1900-1905, Henri Matisse was part of a group called the Fauves, otherwise known as",
-    answer: "Wild Beasts",
-    options: [
-      "Delicate Brush",
-      "Colorful Children",
-      "Wild Beasts",
-      "Joyful Painters",
-    ],
+      "A wild flamingo's color comes from its diet which includes shrimp, plankton, algae, and crustaceans. These foods are all high in ___ which gives them their pink/orange hues.",
+    answer: "Carotene",
+    options: ["Dornic", "Ashwagandha", "Spirulina", "Carotene"],
   },
   {
     // question and answers 4
     numb: 4,
     question:
-      "During Frida Kahlo's first solo exhibit, she was not supposed to attend but instead surprised guests by",
-    answer:
-      "Arriving on a stretcher before being placed in her bed in the middle of the exhibit",
+      "An adult flamingos body will be shorter than the length of it's legs. Leg length of an adult flamingo typically ranges between _____",
+    answer: "30- to 50-inches long",
     options: [
-      "Propelling from the ceiling into the middle of the room",
-      "Dancing into the room surrounded by monkeys",
-      "Arriving on a stretcher before being placed in her bed in the middle of the exhibit",
-      "Disguising herself as a man",
+      "15- to 25-inches long",
+      "30- to 50-inches long",
+      "55- to 65-inches long",
+      "70- to 80-inches long",
     ],
   },
   {
     // question and answers 5
     numb: 5,
-    question:
-      "This artist let her husband take credit for being the creator of her Big Eye paintings. She finally proved to be the true artist by live painting in a court battle",
-    answer: "Margaret Keane",
-    options: [
-      "Barbara Stearn",
-      "Artemesia Gentileschi",
-      "Margaret Keane",
-      "Georgia O'Keefe",
-    ],
+    question: `The word "flamingo" comes from the Spanish and Latin word "flamenco," meaning ____`,
+    answer: "Fire",
+    options: ["Fierce", "Dance", "Flight", "Fire"],
   },
 ];
 
@@ -116,30 +103,22 @@ let timerInterval;
 
 // Declaring variables that select html elements
 const startBtn = document.getElementById("start-btn");
-
 const startContainer = document.getElementById("start-container");
 const questionContainer = document.getElementById("question-container");
-
-const answers = document.getElementById("answers");
-const option1 = document.querySelector(".option1");
-const option2 = document.querySelector(".option2");
-const option3 = document.querySelector(".option3");
-const option4 = document.querySelector(".option4");
-
+const answerContainer = document.getElementById("answerContainer");
+const topBar = document.querySelector(".top-bar");
 const timer = document.querySelector(".timer");
 const countdown = document.getElementById("countdown");
 const goBack = document.getElementById("back");
 const clear = document.getElementById("clear");
-let option = document.querySelectorAll(".option");
-const answerBtn = document.querySelector(".answer-btn");
 
 // Function to start the quiz
 startBtn.onclick = function () {
   startContainer.style.display = "none";
   questionContainer.style.visibility = "visible";
-  answers.style.visibility = "visible";
-  advanceQuestion();
-  // showQuestions();
+  answerContainer.style.visibility = "visible";
+  topBar.style.visibility = "visible";
+  showQuestions();
   beginTimer(secondsLeft);
 };
 
@@ -151,7 +130,7 @@ function showQuestions() {
   for (let i = 0; i < quizQuestions[questionNumber].options.length; i++) {
     let generatedButton = document.createElement("button");
     generatedButton.innerText = quizQuestions[questionNumber].options[i];
-    answers.append(generatedButton);
+    answerContainer.append(generatedButton);
 
     // gives each element it's own data attribute
     // storing data into a html element for each button so that each button could be refered to specifically later
@@ -175,7 +154,7 @@ function showQuestions() {
         console.log(score);
 
         // removes the buttons so that when the new ones are generated the former ones don't remain
-        answers.innerHTML = "";
+        answerContainer.innerHTML = "";
 
         advanceQuestion();
       } else {
@@ -185,7 +164,7 @@ function showQuestions() {
         this.style.backgroundColor = "red";
         this.style.borderColor = "red";
 
-        answers.innerHTML = "";
+        answerContainer.innerHTML = "";
 
         advanceQuestion();
       }
@@ -206,13 +185,16 @@ function advanceQuestion() {
 function endgame() {
   console.log("game is over");
   questionContainer.innerText = "Game Over";
+  answerContainer.style.visibility = "hidden";
 
   clearInterval(timerInterval);
 
   timer.style.display = "none";
   const scoreEl = document.createElement("div");
   scoreEl.innerText = `Your Score: ${secondsLeft}`;
-  answers.appendChild(scoreEl);
+  scoreEl.classList.add = "display-score";
+  console.log(secondsLeft);
+  answerContainer.appendChild(scoreEl);
 }
 
 // Select the timer element by its class
@@ -238,3 +220,7 @@ goBack.onclick = function () {
   startContainer.style.display = "visible";
   questionContainer.style.visibility = "none";
 };
+
+// Quiz sources:
+// https://facts.net/flamingo-facts/
+// https://www.thespruce.com/fun-facts-about-flamingos-385519
